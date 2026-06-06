6d9aef815dd285c4eeece7e5d0079d4af51a822a
ChinmayPalaye <47626129+ChinmayPalaye@users.noreply.github.com>
2026-06-06 20:02:29 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length());
+            sb.append("-");
+            sb.append(str);
+        }
+        // System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> ans = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            StringBuilder lenS = new StringBuilder();
+            // int j = i;
+            while (str.charAt(i) != '-') {
+                lenS.append(str.charAt(i++));
+            }
+            // System.out.println(lenS.toString());
+            int len = lenS.toString() == "" ? 0 : Integer.parseInt(lenS.toString());
+            StringBuilder word = new StringBuilder();
+            i++;
+            // i = j;
+            for (int k = i; k < i + len; k++) {
+                word.append(str.charAt(k));
+            }
+            i += len;
+            ans.add(word.toString());
+        }
+        return ans;
+    }
+}
